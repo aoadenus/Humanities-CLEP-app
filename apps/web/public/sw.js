afe28448fb28e02285a8a7b2bf39b34bde3fe4b4
Adetutu Adenusi <aoadenus@cougarnet.uh.edu>
2026-04-19 22:33:50 -0500

    Disable stale service worker caching

--- apps/web/public/sw.js
+++ apps/web/public/sw.js
@@ -1,38 +1,11 @@
-const CACHE_NAME = "clep-humanities-v1";
-const APP_SHELL = ["/", "/manifest.webmanifest", "/icon.svg"];
-
-self.addEventListener("install", (event) => {
-  event.waitUntil(caches.open(CACHE_NAME).then((cache) => cache.addAll(APP_SHELL)));
+self.addEventListener("install", () => {
+  self.skipWaiting();
 });
 
 self.addEventListener("activate", (event) => {
   event.waitUntil(
-    caches.keys().then((keys) =>
-      Promise.all(
-        keys.filter((key) => key !== CACHE_NAME).map((key) => caches.delete(key)),
-      ),
-    ),
-  );
-});
-
-self.addEventListener("fetch", (event) => {
-  if (event.request.method !== "GET") {
-    return;
-  }
-
-  event.respondWith(
-    caches.match(event.request).then((cached) => {
-      if (cached) {
-        return cached;
-      }
-
-      return fetch(event.request)
-        .then((response) => {
-          const copy = response.clone();
-          caches.open(CACHE_NAME).then((cache) => cache.put(event.request, copy));
-          return response;
-        })
-        .catch(() => caches.match("/"));
+    self.registration.unregister().then(() => self.clients.matchAll()).then((clients) => {
+      clients.forEach((client) => client.navigate(client.url));
     }),
   );
 });
--- apps/web/src/components/pwa-register.tsx
+++ apps/web/src/components/pwa-register.tsx
@@ -5,7 +5,15 @@ import { useEffect } from "react";
 export function PwaRegister() {
   useEffect(() => {
     if ("serviceWorker" in navigator) {
-      navigator.serviceWorker.register("/sw.js").catch(() => undefined);
+      void navigator.serviceWorker.getRegistrations().then((registrations) => {
+        void Promise.all(registrations.map((registration) => registration.unregister()));
+      });
+    }
+
+    if ("caches" in window) {
+      void caches.keys().then((keys) => {
+        void Promise.all(keys.map((key) => caches.delete(key)));
+      });
     }
   }, []);
 
